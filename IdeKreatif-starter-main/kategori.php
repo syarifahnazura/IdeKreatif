b673246ac97b2052b7326fb91a99be0e664e3886
syarifahnazura <nazurasyarifah@gmail.com>
2025-02-08 12:34:14 +0700

    menambahkan hapus data kategori

--- IdeKreatif-starter-main/kategori.php
+++ IdeKreatif-starter-main/kategori.php
@@ -57,7 +57,29 @@ include '.includes/toast_notification.php';
                             </td>
                         </tr>
                         <!-- Modal untuk Hapus Data Kategori -->
-
+                        <div class="modal fade" id="deleteCategory_<?= $category['category_id']; ?>" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-tittle">Hapus Kategori</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="proses_kategori.php" method="POST">
+                                            <div>
+                                                <p>Tindakan ini tidak bisa dibatalkan.</p>
+                                                <input type="hidden" name="catID" value="<?=$category['category_id']; ?>">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-outline-secondary"
+                                                data-bs-dismiss="modal">Batal</button>
+                                                <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <!-- Modal untuk Update Dta Kategori -->
 
                         <?php endwhile; ?>
