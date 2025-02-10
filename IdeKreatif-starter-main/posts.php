afd01bc9992719005180f6f13182bd461df55174
syarifahnazura <nazurasyarifah@gmail.com>
2025-02-10 10:06:53 +0700

    membuat halaman posts

---
+++ IdeKreatif-starter-main/posts.php
@@ -0,0 +1,57 @@
+<?php
+// menyertakan header halaman
+include '.includes/header.php';
+?>
+<div class="container-xxl flex-grow-1 container-p-y">
+    <!-- judul halaman --> 
+     <div class="row">
+        <!-- form untuk menmbahkan postingn baru -->
+         <div class="col-md-10">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <form method="POST" action="proses_post.php"
+                    enctype="multipart/form-data">
+                    <!-- input untuk judul postingan --> 
+                     <div class="mb-3">
+                        <label for="post_title" class="form-label">Judul Postingan</label>
+                        <input type="text" class="form-control"name="post_title" required>
+                    </div>
+                    <!-- input untuk mengunggah gambah --> 
+                     <div class="mb-3">
+                        <label for="formFile" class="form-label">Unggah Gambar</label>
+                        <input class="form-control" type="file" name="image" accept="image/*" />
+                    </div>
+                    <!-- dropdown untuk memilih kategori --> 
+                     <div class="mb-3">
+                        <label for="category_id" class="form-label">Kategori</label>
+                        <select class="form-select" name="category_id" required>
+                            <!-- mengambil data kategori dari database untuk mengisi opsi dropdown --> 
+                             <option value="" selected disabled>Pilih Salah Satu</option>
+                             <?php
+                             $query = "SELECT * FROM categories"; // query untuk mengambil data kategori
+                             $result = $conn->query($query); // menjalankan query
+                             if ($result->num_rows > 0) { // jika terdapat data kategori
+                                while ($row = $result->fetch_assoc()) { // iterasi setiap kode
+                                    echo "<option value='" . $row["category_id"] . "'>" . $row["category_name"] . "</option>"; 
+                                    }
+                                }
+                                ?>
+                                </select>
+                            </div>
+                             <!-- textarea untuk konten postingan -->
+                              <div class="mb-3">
+                                <label for="content" class="form-label">Konten</label>
+                                <textarea class="form-control" id="content" name="content" required></textarea>
+                            </div>
+                            <!-- tombol submit -->
+                             <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                            </form>
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+                            <?php
+                            // menyertakan footer halaman 
+                            include '.includes/footer.php';
+                            ?>
